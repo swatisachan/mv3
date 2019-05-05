dddad1b55c494cd393f97dfda4c439a6aef9dfff
swati <swatiinfosys86@gmail.com>
2019-05-05 02:34:18 -0700

    f

---
+++ mv2/src/test/java/sele1/mv2/NewTest2.java
@@ -0,0 +1,13 @@
+package sele1.mv2;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  
+	  assertEquals("swati", "swati");
+  }
+}
