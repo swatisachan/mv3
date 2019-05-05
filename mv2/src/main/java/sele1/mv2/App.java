4c2d063fa0ed138df60d3965a71fe72a56ec70fc
swati <swatiinfosys86@gmail.com>
2019-05-05 02:33:52 -0700

    f

---
+++ mv2/src/main/java/sele1/mv2/App.java
@@ -0,0 +1,13 @@
+package sele1.mv2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
